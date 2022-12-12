c7576d1761ba3cfccc99b74142e92e7f8401c5da
Josue Contreras <estavesyano@gmail.com>
2022-12-12 18:39:07 -0500

    Progressive Web Apps con Next.js 12: next/pwa y su nuevas versiones

--- next.config.js
+++ next.config.js
@@ -1,6 +1,53 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['api.lorem.space', 'm.media-amazon.com', 'placeimg.com'],
-  }
-}
+  },
+};
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
+
+// const runtimeCaching = require('next-pwa/cache');
+// const withPWA = require('next-pwa')({
+//     dest: 'public',
+//     register: true,
+//     mode: 'production',
+//     disable: true,
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest\.json$/],
+// });
+
+// module.exports = withPWA({
+//   ...withPWA,
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['api.lorem.space', 'm.media-amazon.com', 'placeimg.com'],
+//   },
+// });
+
+// const withPWA = require('next-pwa');
+// const runtimeCaching = require('next-pwa/cache');
+
+// module.exports = withPWA({
+//   pwa: {
+//     dest: 'public',
+//     register: true,
+//     mode: 'production',
+//     disable: true,
+//     runtimeCaching,
+//     buildExcludes: [/middleware-manifest\.json/],
+//   },
+//   reactStrictMode: true,
+//   images: {
+//     domains: ['api.lorem.space', 'm.media-amazon.com', 'placeimg.com'],
+//   },
+// });
